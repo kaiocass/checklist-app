3463ecd896744472110c3d57d2269b088af1e797
Kaio Cassiano <imkaiocass@gmail.com>
2023-10-15 19:31:31 +0100

    Added category content

--- src/app/category/category.component.ts
+++ src/app/category/category.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Category } from '../_models/category';
+
+export const CATEGORY_DATA = [
+  {name: 'Educação', guid: 'aaa-bbb-ccc-ddd'},
+  {name: 'Saúde', guid: 'aaa-bbb-ccc-ddd'},
+  {name: 'Trabalho', guid: 'aaa-bbb-ccc-ddd'},
+  {name: 'Outros', guid: 'aaa-bbb-ccc-ddd'},
+]
 
 @Component({
   selector: 'app-category',
   templateUrl: './category.component.html',
   styleUrls: ['./category.component.css']
 })
-export class CategoryComponent {
+export class CategoryComponent implements OnInit {
+
+  public displayedColumns: string[] = ['id', 'name', 'actions'];
+  public dataSource: Category[] = CATEGORY_DATA;
+
+  constructor() {}
+
+  ngOnInit(): void {  }
+
+  public createNewCategory() { }
+
+  public editCategory(category: Category) { }
+
+  public deleteCategory(category: Category) { }
 
 }
--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,17 +1,19 @@
 import { NgModule } from '@angular/core';
 
+import { FormsModule } from '@angular/forms';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatListModule } from '@angular/material/list';
-import {MatDividerModule} from '@angular/material/divider';
-
-import { FormsModule } from '@angular/forms';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatCardModule } from '@angular/material/card';
+import { MatTableModule } from '@angular/material/table';
 
 @NgModule({
   imports: [
+    FormsModule,
     MatToolbarModule,
     MatButtonModule,
     MatIconModule,
@@ -19,9 +21,11 @@ import { FormsModule } from '@angular/forms';
     MatCheckboxModule,
     MatListModule,
     MatDividerModule,
-    FormsModule,
+    MatCardModule,
+    MatTableModule,
   ],
   exports: [
+    FormsModule,
     MatToolbarModule,
     MatButtonModule,
     MatIconModule,
@@ -29,7 +33,8 @@ import { FormsModule } from '@angular/forms';
     MatCheckboxModule,
     MatListModule,
     MatDividerModule,
-    FormsModule,
+    MatCardModule,
+    MatTableModule,
   ],
   providers: [],
 })
